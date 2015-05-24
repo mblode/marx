6048cb87bd1be7ea3a5a6221662cc83b4927dd73
mblode <m.blode@gmail.com>
2015-05-24 12:51:26 +1000

    Gulp and things

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var $g = require('gulp-load-plugins')();
+var g = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['styles'], function() {
   browserSync({
     server: {
       baseDir: './'
@@ -14,44 +14,29 @@ gulp.task('browser-sync', function() {
 
 gulp.task('scss-lint', function(){
   return gulp.src('scss/**/*.scss')
-    .pipe($g.scssLint());
+    .pipe(g.scssLint());
 });
 
-gulp.task('sass', ['scss-lint'], function() {
+gulp.task('styles', ['scss-lint'], function() {
   return gulp.src('scss/*.scss')
-    .pipe($g.sourcemaps.init())
-      .pipe($g.sass({
+    .pipe(g.sourcemaps.init())
+      .pipe(g.sass({
         precision: 10,
         onError: console.error.bind(console, 'Sass error:')
       }))
-      .pipe($g.autoprefixer(['last 2 versions, > 5%'], {
-        cascade: true
-      }))
-    .pipe($g.sourcemaps.write('.'))
+      .pipe(g.autoprefixer('last 2 versions'))
+      .pipe(g.minifyCss())
+      .pipe(g.rename({suffix: '.min'}))
+    .pipe(g.sourcemaps.write('.'))
+    .pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('css-min', ['sass'], function() {
-  return gulp.src(['css/*.css', '!css/*.min.css'])
-  .pipe($g.sourcemaps.init())
-    .pipe($g.minifyCss())
-    .pipe($g.rename({
-      suffix: '.min'
-    }))
-  .pipe($g.sourcemaps.write('.'))
-  .pipe(gulp.dest('css/'));
-});
-
-gulp.task('serve', ['scss-lint', 'sass', 'css-min'], function() {
-
-  browserSync({
-    server: './'
-  });
-
-  gulp.watch('scss/**/*.scss', ['sass', 'css-min', reload]);
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['styles', reload]);
   gulp.watch('*.html').on('change', reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['scss-lint', 'styles', 'browser-sync', 'watch']);
 
-gulp.task('build', ['scss-lint', 'sass', 'css-min']);
+gulp.task('build', ['scss-lint', 'styles']);
